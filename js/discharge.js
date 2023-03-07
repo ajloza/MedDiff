6ecdfebb9c111248d5681251157adcffb907c7a8
Andrew Loza <77357202+ajloza@users.noreply.github.com>
2023-03-06 22:10:42 -0500

    typo fixes and icon change

--- js/discharge.js
+++ js/discharge.js
@@ -263,8 +263,9 @@ function matchTailNoI() {
 function matchTailI(matchMed) {
     let str =   "<td class = 'col-2 text-center'>"+
     "<span class='c-dyellow' data-bs-toggle='tooltip' data-bs-placement='top' data-bs-original-title='"+
-    matchMed.tokens.join(" ")+"'>"+    
-    "<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-info-square-fill' viewBox='0 0 16 16'><path d='M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.93 4.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2z'/></svg>"+
+    matchMed.tokens.join(" ")+"'>"+
+    "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-prescription2' viewBox='0 0 16 16'>    <path d='M7 6h2v2h2v2H9v2H7v-2H5V8h2V6Z'/>    <path d='M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v10.5a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 3 14.5V4a1 1 0 0 1-1-1V1Zm2 3v10.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V4H4ZM3 3h10V1H3v2Z'/>  </svg>"    
+    // "<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-info-square-fill' viewBox='0 0 16 16'><path d='M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.93 4.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2z'/></svg>"+
     "</span>"+
     "</td></tr>"
     return str
